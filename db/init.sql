df8c77df896a090d0335944cd4f68a8ee6ffc5ac
Michele Vigilante <michelevig@protonmail.com>
2024-07-28 16:54:44 +0200

    setup test environment with unixodbc driver

--- db/init.sql
+++ db/init.sql
@@ -1,17 +1,28 @@
 CREATE TABLE temp_readings (
-    location_id INTEGER,
-    timestamp TIMESTAMP,
-    temperature_c FLOAT,
-    humidity FLOAT,
+    location_id       INTEGER,
+    timestamp         TIMESTAMP,
+    temperature_c     FLOAT,
+    humidity          FLOAT,
     weather_condition VARCHAR(50)
 );
 
 CREATE TABLE temp_readings_empty (
-    location_id INTEGER,
-    timestamp TIMESTAMP,
-    temperature_c FLOAT,
-    humidity FLOAT,
+    location_id       INTEGER,
+    timestamp         TIMESTAMP,
+    temperature_c     FLOAT,
+    humidity          FLOAT,
     weather_condition VARCHAR(50)
+);
+
+CREATE TABLE temp_readings_aggregated (
+    date            DATE,
+    location_id     INTEGER,
+    min_temp_c      FLOAT,
+    min_humidity    FLOAT,
+    max_temp_c      FLOAT,
+    max_humidity    FLOAT,
+    avg_temp_c      FLOAT,
+    avg_humidity    FLOAT
 );
 
 COPY temp_readings FROM '/opt/temp_readings_jan_2024.csv' DELIMITER ',' CSV HEADER;
